f841d2f5e697fb8d0e75d2dbb26fd8c776f96a9a
PointyHatBeast <istosavolainen@gmail.com>
2015-04-14 12:07:38 +0300

    DLL commit

---
+++ RescalingDLL/RescalingDLL.cpp
@@ -0,0 +1,6 @@
+// RescalingDLL.cpp : Defines the exported functions for the DLL application.
+//
+
+#include "stdafx.h"
+
+
---
+++ RescalingDLL/RescalingFuncs.cpp
@@ -0,0 +1,19 @@
+#include "stdafx.h"
+#include "RescalingFuncs.h"
+
+namespace RescalingFuncs{
+	class RescalingFuncs{
+		RescalingFuncs()
+		{
+		}
+
+
+		~RescalingFuncs()
+		{
+		}
+		static void RESCALINGFUNCSDLL_API Downscale(char name, int size){
+
+
+		}
+	};
+}
