ddf8c35c9b38c55f2ebed610e00a50487ed14ba6
Isto-Pekka Savolainen <istosavolainen@gmail.com>
2015-04-16 18:02:29 +0300

    Changes

--- RescalingDLL/stdafx.h
+++ RescalingDLL/stdafx.h
@@ -2,15 +2,46 @@
 // or project specific include files that are used frequently, but
 // are changed infrequently
 //
-
 #pragma once
 
+#ifndef DLLclass
+#define DLLclass
+
 #include "targetver.h"
+
+#include <cstdint>
+#include <objidl.h>
+#include <Gdiplus.h>
+#include <GdiPlusImageCodec.h>
+#include <string>
+#include <fstream>
 
 #define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
 // Windows Header Files:
 #include <windows.h>
 
+#ifdef RESCALINGFUNCSDLL_EXPORTS
+#define RESCALINGFUNCSDLL_API __declspec(dllexport)
+#else
+#define RESCALINGFUNCSDLL_API __declspec(dllimport)
+#endif
 
 
-// TODO: reference additional headers your program requires here
+extern "C"
+{
+	RESCALINGFUNCSDLL_API void Downscale(char nimi, int size);
+
+	RESCALINGFUNCSDLL_API int GetEncoderClsid(const WCHAR* form, CLSID* pClsid);
+}
+
+//class RescalingFuncs
+//{
+//public:
+//	// creates copy of a picture and rescales it
+//	
+//private:
+//	
+//};
+
+
+#endif 
--- ResizePicGit/ResizePicGit.cpp
+++ ResizePicGit/ResizePicGit.cpp
@@ -2,10 +2,64 @@
 //
 
 #include "stdafx.h"
+#include "RescaleFuncs.h"
 
 
-int _tmain(int argc, _TCHAR* argv[])
-{
-	return 0;
+namespace RescalingFunds{
+
+	void rescalefuns::Downscale(char nimi, int size){
+		// Prepare GDIplus
+		Gdiplus::GdiplusStartupInput gdiplusStartupInput;
+		ULONG_PTR gdiplusToken;
+		Gdiplus::GdiplusStartup(&gdiplusToken, &gdiplusStartupInput, 0);
+
+		// load the image from the disk
+		Gdiplus::Bitmap* original;
+		wchar_t const* name = L"D:\Kuvat\gitproj\test.jpg";
+		wchar_t const* name2 = L"D:\Kuvat\gitproj\test2.jpg";
+		original->FromFile(name);
+
+		UINT o_height = original->GetHeight();
+		UINT o_width = original->GetWidth();
+
+		INT n_width = 150;
+		INT n_height = 150;
+
+		Gdiplus::Bitmap* newBitmap = new Gdiplus::Bitmap(n_width, n_height, original->GetPixelFormat());
+		Gdiplus::Graphics graphics(newBitmap);
+		graphics.DrawImage(original, 0, 0, n_width, n_height);
+
+		CLSID jpgCLsid;
+		if (GetEncoderClsid(L"image/jpeg", &jpgCLsid) > -1){
+			newBitmap->Save(name2, &jpgCLsid);
+		}
+
+		// Stop Gdiplus
+		Gdiplus::GdiplusShutdown(gdiplusToken);
+	}
+
+	int rescalefuns::GetEncoderClsid(const WCHAR* form, CLSID* pClsid){
+
+		UINT num;
+		UINT size;
+		Gdiplus::ImageCodecInfo* pImageCodecInfo = NULL;
+		Gdiplus::GetImageEncodersSize(&num, &size);
+		if (size == 0)
+			return -1;
+		pImageCodecInfo = (Gdiplus::ImageCodecInfo*)(malloc(size));
+		if (pImageCodecInfo == NULL)
+			return -1;
+		Gdiplus::GetImageEncoders(num, size, pImageCodecInfo);
+		for (UINT j = 0; j < num; j++)
+		{
+			if (wcscmp(pImageCodecInfo[j].MimeType, form) == 0)
+			{
+				*pClsid = pImageCodecInfo[j].Clsid;
+				free(pImageCodecInfo);
+				return j;
+			}
+		}
+		free(pImageCodecInfo);
+		return -1;
+	}
 }
-
