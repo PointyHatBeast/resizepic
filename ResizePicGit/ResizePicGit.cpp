a8fe50daa5d945610446ceab016b79622c07de39
Isto-Pekka Savolainen <istosavolainen@gmail.com>
2015-04-19 00:47:36 +0300

    dded some files

--- ResizePicGit/ResizePicGit.cpp
+++ ResizePicGit/ResizePicGit.cpp
@@ -5,69 +5,69 @@
 #include "RescaleFuncs.h"
 
 
-namespace RescalingFunds{
+//namespace RescalingFunds{
+using namespace RescalingFunds;
 
-	void rescalefuns::Downscale(string path, string nimi, int size){
-		// Prepare GDIplus
-		Gdiplus::GdiplusStartupInput gdiplusStartupInput;
-		ULONG_PTR gdiplusToken;
-		Gdiplus::GdiplusStartup(&gdiplusToken, &gdiplusStartupInput, 0);
+void rescalefuns::Downscale(string path, string nimi, int size){
+	// Prepare GDIplus
+	Gdiplus::GdiplusStartupInput gdiplusStartupInput;
+	ULONG_PTR gdiplusToken;
+	Gdiplus::GdiplusStartup(&gdiplusToken, &gdiplusStartupInput, 0);
 
-		//tehdään kuvan polusta yhteensopiva Gdiplussan funktioille
-		string originalfilepath = path + nimi;
-		wstring originalpathwide(originalfilepath.begin(), originalfilepath.end());
+	//tehdään kuvan polusta yhteensopiva Gdiplussan funktioille
+	string originalfilepath = path + nimi;
+	wstring originalpathwide(originalfilepath.begin(), originalfilepath.end());
 
 
-		//wchar_t const* name = L"D:\\Kuvat\\gitproj\\test.jpg";
-		wchar_t const* name = originalpathwide.c_str();
-		wchar_t const* name2 = L"D:\\Kuvat\gitproj\\test2.jpg";
-		// load the image from the disk
-		Gdiplus::Bitmap* original = Gdiplus::Bitmap::FromFile(name);
+	wchar_t const* name = L"D:\\Kuvat\\gitproj\\test.jpg";
+	//wchar_t const* name = originalpathwide.c_str();
+	wchar_t const* name2 = L"D:\\Kuvat\gitproj\\test2.jpg";
+	// load the image from the disk
+	Gdiplus::Bitmap* original = Gdiplus::Bitmap::FromFile(name);
 
-		UINT o_height = original->GetHeight();
-		UINT o_width = original->GetWidth();
+	UINT o_height = original->GetHeight();
+	UINT o_width = original->GetWidth();
 
-		INT n_width = 150;
-		INT n_height = 150;
+	INT n_width = 150;
+	INT n_height = 150;
 
-		Gdiplus::Bitmap* newBitmap = new Gdiplus::Bitmap(n_width, n_height, original->GetPixelFormat());
-		Gdiplus::Graphics graphics(newBitmap);
-		graphics.DrawImage(original, 0, 0, n_width, n_height);
+	Gdiplus::Bitmap* newBitmap = new Gdiplus::Bitmap(n_width, n_height, original->GetPixelFormat());
+	Gdiplus::Graphics graphics(newBitmap);
+	graphics.DrawImage(original, 0, 0, n_width, n_height);
 
-		CLSID jpgCLsid;
-		if (GetEncoderClsid(L"image/jpeg", &jpgCLsid) > -1){
-			newBitmap->Save(name2, &jpgCLsid);
-		}
-		
-		delete original;
-		// Stop Gdiplus
-		Gdiplus::GdiplusShutdown(gdiplusToken);
-
-		return;
+	CLSID jpgCLsid;
+	if (GetEncoderClsid(L"image/jpeg", &jpgCLsid) > -1){
+		newBitmap->Save(name2, &jpgCLsid);
 	}
 
-	int rescalefuns::GetEncoderClsid(const WCHAR* form, CLSID* pClsid){
-
-		UINT num;
-		UINT size;
-		Gdiplus::ImageCodecInfo* pImageCodecInfo = NULL;
-		Gdiplus::GetImageEncodersSize(&num, &size);
-		if (size == 0)
-			return -1;
-		pImageCodecInfo = (Gdiplus::ImageCodecInfo*)(malloc(size));
-		if (pImageCodecInfo == NULL)
-			return -1;
-		Gdiplus::GetImageEncoders(num, size, pImageCodecInfo);
-		for (UINT j = 0; j < num; j++)
-		{
-			if (wcscmp(pImageCodecInfo[j].MimeType, form) == 0)
-			{
-				*pClsid = pImageCodecInfo[j].Clsid;
-				free(pImageCodecInfo);
-				return j;
-			}
-		}
-		free(pImageCodecInfo);
-		return -1;
-	}
+	delete original;
+	delete newBitmap;
+	// Stop Gdiplus
+	Gdiplus::GdiplusShutdown(gdiplusToken);
 }
+
+int rescalefuns::GetEncoderClsid(const WCHAR* form, CLSID* pClsid){
+
+	UINT num;
+	UINT size;
+	Gdiplus::ImageCodecInfo* pImageCodecInfo = NULL;
+	Gdiplus::GetImageEncodersSize(&num, &size);
+	if (size == 0)
+		return -1;
+	pImageCodecInfo = (Gdiplus::ImageCodecInfo*)(malloc(size));
+	if (pImageCodecInfo == NULL)
+		return -1;
+	Gdiplus::GetImageEncoders(num, size, pImageCodecInfo);
+	for (UINT j = 0; j < num; j++)
+	{
+		if (wcscmp(pImageCodecInfo[j].MimeType, form) == 0)
+		{
+			*pClsid = pImageCodecInfo[j].Clsid;
+			free(pImageCodecInfo);
+			return j;
+		}
+	}
+	free(pImageCodecInfo);
+	return -1;
+}
+//}
